45ece81176f4486e3d54cf259b48635768ce3c18
yannisl <y.lazarides@habtoorspecon.com>
2015-12-07 14:33:23 +0400

    various

---
+++ china/china/conv.bat
@@ -0,0 +1,27 @@
+@echo off
+ 
+rem 1. Download & install ImageMagick from http://www.imagemagick.org/script/binary-releases.php#windows. 
+rem This is a command line utility to work with images. No ads, toolbars and other crap. It is freeware and open source.
+rem You need file named ImageMagick-<version number>-windows-static.exe
+rem 
+rem 2. Create new file conv.bat with this snippet inside in a folder with images.
+rem This will automatically trim all images in this folder and all its subfolders.
+ 
+rem Settings :
+ 
+rem Images extension
+rem jp*g will match both .jpg's and .jpeg's
+set extension=jp*g
+ 
+rem How much tolerance when matching color in percents
+rem Larger tolerance = broader color range is regarded same and trimmed
+rem Zero will mach only one exact color, use it for perfectly clean images in lossless formats (png, tif etc)
+rem Use small value for clean images in jpegs, 1%%-5%% is a good start
+set tolerance=1%%
+ 
+rem More info on parameters - see http://www.imagemagick.org/Usage/crop/#trim
+ 
+for /R %%i in (*.%extension%) do ( 
+	echo Trimming %%i
+	convert "%%i" -fuzz "%tolerance%" -trim +repage "%%i" 
+)
